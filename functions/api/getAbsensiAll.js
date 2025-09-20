9280186cc84c86d0a4d24b5db21c739ae3e003c8
rqhkmtb <rqhkmtb@outlook.com>
2025-09-20 14:17:13 +0700

    Batch replace: "muqoddammtb" -> "rqhkmtb" | getAbsensiAll.js | hits=1

--- functions/api/getAbsensiAll.js
+++ functions/api/getAbsensiAll.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type,Authorization",
 };
 
-const OWNER_REPO = "muqoddammtb/server";
+const OWNER_REPO = "rqhkmtb/server";
 const DEFAULT_BRANCH = "main";
 
 const ghHeaders = (token) => ({
