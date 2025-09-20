895ab2e3ac0e0a0fdcdeaaf1ffb89493ed4a84ab
rqhkmtb <rqhkmtb@outlook.com>
2025-09-20 14:17:42 +0700

    Batch replace: "muqoddammtb" -> "rqhkmtb" | pindahRosterKelas.js | hits=1

--- functions/api/pindahRosterKelas.js
+++ functions/api/pindahRosterKelas.js
@@ -1,7 +1,7 @@
 // functions/api/pindahRosterKelas.js
 // Cloudflare Pages Functions (ESM)
 
-const DEFAULT_REPO   = "muqoddammtb/server";
+const DEFAULT_REPO   = "rqhkmtb/server";
 const DEFAULT_BRANCH = "main";
 
 const enc = new TextEncoder();
