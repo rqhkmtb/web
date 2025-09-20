5bab7720066c150a7202f1a68b27d624f74e7c61
rqhkmtb <rqhkmtb@outlook.com>
2025-09-20 14:17:32 +0700

    Batch replace: "muqoddammtb" -> "rqhkmtb" | getTanggalRange.js | hits=1

--- functions/api/getTanggalRange.js
+++ functions/api/getTanggalRange.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-const OWNER = "muqoddammtb";
+const OWNER = "rqhkmtb";
 const REPO  = "server";
 const BRANCH = "main";
 
