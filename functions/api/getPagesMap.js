3dffbed472653fd736e6bb16520bfff21a9657f5
rqhkmtb <rqhkmtb@outlook.com>
2025-09-20 14:17:27 +0700

    Batch replace: "muqoddammtb" -> "rqhkmtb" | getPagesMap.js | hits=1

--- functions/api/getPagesMap.js
+++ functions/api/getPagesMap.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-const OWNER_REPO = "muqoddammtb/server";
+const OWNER_REPO = "rqhkmtb/server";
 const FILE_PATH = "getPagesMap.json";
 const BRANCH = "main";
 
